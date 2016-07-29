dbe3579528273c1e3374b22198b72d8ac2ba75c2
Hagai Cohen <hagai.co@gmail.com>
2016-07-29 12:50:23 +0300

    Revert "Version 0.0.2 => Added Autostart + CBQueue support"
    This reverts commit b94d890675a7186c826b355b998702439c140638.

--- modules/AsyncEventEmitter.ts
+++ modules/AsyncEventEmitter.ts
@@ -1,27 +1,8 @@
 "use strict";
 
 import { EventEmitter } from "events";
-import { CBQueue } from "CBQueue";
 
 export class AsyncEventEmitter extends EventEmitter {
-    private _cbq: CBQueue<any>;
-
-    constructor(autoStart: boolean = true) {
-        super();
-        this._cbq = new CBQueue();
-
-        if ( true === autoStart ) {
-            this.start();
-        }
-    }
-
-    /**
-     * Starts the event emitter incase autoStart was set to false.
-     */
-    public start(): Promise<void> {
-        return this._cbq.start();
-    }
-
     /**
      * Synchronously calls each of the listeners registered for the event named eventName,
      * in the order they were registered, passing the supplied arguments to each.
@@ -31,17 +12,15 @@ export class AsyncEventEmitter extends EventEmitter {
      * @returns Promise that will be resolved as array of return values of all listeners.
      */
     public emitAsync<T>(eventName: string, ...args: any[]): Promise<T[]> {
-        return this._cbq.push(() => {
-            return Promise.all<T>(this.listeners(eventName)
-            .map((listener: (...args: any[]) => T | Promise<T>): Promise<T> => {
-                return new Promise<T>((resolve, reject) => {
-                    try {
-                        resolve(listener(...args));
-                    } catch (e) {
-                        reject(e);
-                    }
-                });
-            }));
-        });
+        return Promise.all<T>(this.listeners(eventName)
+        .map((listener: (...args: any[]) => T | Promise<T>): Promise<T> => {
+            return new Promise<T>((resolve, reject) => {
+                try {
+                    resolve(listener(...args));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        }));
     }
 }
